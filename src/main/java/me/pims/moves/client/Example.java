0abfd1a2f71963b3536d52c6d38bbe24eebd8e16
tim <tim@fewagainstmany.com>
2014-02-25 21:05:03 -0800

    Making Moves final

--- src/main/java/me/pims/moves/client/Example.java
+++ src/main/java/me/pims/moves/client/Example.java
@@ -12,7 +12,7 @@ public class Example {
 
         final String token = System.getenv("ACCESS_TOKEN");
 
-        Moves moves = new MovesBuilder().setToken(token).build();
+        final Moves moves = new MovesBuilder().setToken(token).build();
 
         final Profile profile = moves.profile();
 
