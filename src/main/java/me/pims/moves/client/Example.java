4a40afe4cd0e09db03abb69d0eaf8e8d409f0427
Tim Bisson <bissont@mac.com>
2016-06-17 00:34:55 -0700

    Got access control to work and load my history with my dates

--- src/main/java/me/pims/moves/client/Example.java
+++ src/main/java/me/pims/moves/client/Example.java
@@ -10,20 +10,21 @@ public class Example {
 
     public static void main(String[] args) throws Exception {
 
+        // On mac set access token in .profile with launchctl setenve VAR val
         final String token = System.getenv("ACCESS_TOKEN");
 
         final Moves moves = new MovesBuilder().setToken(token).build();
 
         final Profile profile = moves.profile();
 
-        final List<Summary> summary = moves.summary("2013-11-15");
-        final List<Summary> summaryBetween = moves.summary("2013-11-14", "2013-11-15");
-        final List<Summary> past30days = moves.summary(30);
+		final List<Summary> summary = moves.summary("2016-05-15");
+		final List<Summary> summaryBetween = moves.summary("2016-06-14", "2016-06-15");
+		final List<Summary> past30days = moves.summary(30);
 
-        final List<Summary> activities = moves.activities("2013-11-14");
-        final List<Summary> storyline = moves.storyline("2013-11-14");
-        final List<Summary> places = moves.places("2013-11-14");
-
+		final List<Summary> activities = moves.activities("2016-06-14");
+		final List<Summary> storyline = moves.storyline("2016-06-14");
+		final List<Summary> places = moves.places("2016-06-14");
+		        
         final List<ActivityDescription> descriptions = moves.listOfActivities();
 
         System.out.println(summary);
